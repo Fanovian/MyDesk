d7c767c0b5fe9662a5d5cb58ab17d5c17aec3fb3
Frankoxer <frankoxer@outlook.com>
2024-12-20 20:25:26 +0800

    Initial Commit

---
+++ MyDeskTests/MyDeskTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyDeskTests.swift
+//  MyDeskTests
+//
+//  Created by Frankoxer on 2024/12/20.
+//
+
+import Testing
+@testable import MyDesk
+
+struct MyDeskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
